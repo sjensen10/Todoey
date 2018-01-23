d6ae0aea0f6b8e37c95929b1a8fdc27d89f55a35
Sam Jensen <samjensen@Sams-MacBook-Pro.local>
2018-01-23 16:10:39 -0600

    Updated initial todo list with hardcoded list

---
+++ Todoey/ToDoListViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Sam Jensen on 1/23/18.
+//  Copyright © 2018 SamJensen. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buggy Eggos", "Destor Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //Mark - Tableview DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    // Mark - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath,  animated: true)
+    }
+    
+  
+    
+    
+    
+
+
+}
+
